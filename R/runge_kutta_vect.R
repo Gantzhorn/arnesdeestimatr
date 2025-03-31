dd5d38a4ca9644d242689637a31b5c6b10330bde
Gantzhorn <andersgantzhorn@gmail.com>
2025-03-31 21:11:22 +0200

    Change package name. Update documentation of runge_kutta_vect method

--- R/runge_kutta_vect.R
+++ R/runge_kutta_vect.R
@@ -2,18 +2,18 @@
 #'
 #' This function implements the **Runge-Kutta 4th Order (RK4)** method for solving
 #' ordinary differential equations (ODEs). It is designed to handle **vectorized**
-#' input for initial conditions `x0` and `y0`. The function computes
-#' the next value of `y0`, i.e. the value at `x0 + h` for all entries in `x0`
-#'  based on a given function `func`, step size `h`, and number of sub-steps `n`.
+#' input for initial conditions `x0` and `y0`. The function gives the approximation
+#' to the solution of the initial value problem at `x0 + h` for all starting positions `x0`
+#' based on the provided function, `func`, a step-size `h`, and number of sub-steps `n`.
 #'
-#' @param x0 A numeric vector of initial values for the independent variable. It can handle vectorized input.
-#' @param y0 A numeric vector of initial values for the dependent variable. It can handle vectorized input.
+#' @param x0 A numeric vector of initial values for the independent variable.
+#' @param y0 A numeric vector of initial values for the dependent variable.
 #' @param h A numeric scalar that defines the step size for the Runge-Kutta method.
-#' @param func A function that governs the derivative at a given point. It should take `x0` and `y0` as arguments.
-#' @param n An integer representing the number of sub-steps to iterate through. For our applications n = 1 is typically sufficient.
+#' @param func A function that describes the derivative in the point some point `(x,y)`. It should take `x0` and `y0` as arguments and be vectorized
+#' @param n An integer representing the number of sub-steps to iterate through. Setting n = 1 is typically sufficient in estimation applications.
 #'
-#' @return A numeric vector of estimated values of the dependent variable after `n` iterations.
-#' The output will have the same length as `y0`.
+#' @return A numeric vector of estimated values for the function described `func`, i.e. the ODE, evaluated at the points `x0 + h`.
+#' The output will have the same length as `x0` and `y0`.
 #'
 #' @export
 #'
